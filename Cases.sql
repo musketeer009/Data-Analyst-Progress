aea59a2aef9c8269e4a31a0d4c4f2c3e07eed371
musketeer009 <92170052+musketeer009@users.noreply.github.com>
2025-03-02 20:01:04 +0530

    Cases.sql

--- Cases.sql
+++ Cases.sql
@@ -7,7 +7,7 @@ select name,
 case department
 	when 'sales' then 'sales team'
     else 'other'
-end as employee_name from Employees;
+end as employee_name from employees;
 
 -- Basic case for sales & marketing team
 select name, 
@@ -15,7 +15,7 @@ case department
 	when 'sales' then 'sales team'
     when 'marketing' then 'marketing team'
     else 'other'
-end as employee_name from Employees;
+end as employee_name from employees;
 
 -- Cases with respect to age
 select name, 
@@ -23,7 +23,7 @@ case
 	when age < 30 then 'Young'
     when age <= 30 and age <= 40 then 'mid-aged'
     else 'Senior'
-end as employee_name from Employees;
+end as employee_name from employees;
 
 
 -- Nested Cases
@@ -36,7 +36,7 @@ case
 		end
 	when age >= 30 and age <= 40 then 'Middle'
     else 'Senior'
-end as employee_name from Employees;
+end as employee_name from employees;
 
 
 
