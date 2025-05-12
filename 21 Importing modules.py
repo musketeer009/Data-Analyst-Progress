89aeaa9fa69a2042a55a1e715690cd59ba71b30c
musketeer009 <92170052+musketeer009@users.noreply.github.com>
2025-05-12 20:05:50 +0530

    Add files via upload

---
+++ 21 Importing modules.py
@@ -0,0 +1,24 @@
+import Game20
+
+print(dir(user))
+
+a1= Game20.user("shubham", 22)
+
+print("#"*20)
+print(dir(a1))
+print("#"*20)
+
+a1.info()
+
+print("_"*20)
+
+
+
+a2= Game20.user("Vikash",21)
+a2.info()
+
+print("_"*20)
+
+b = Game20.user("Villain", 110)
+print(b)
+print("_"*20)
